edb51a798859b19848f6b0e99d0785d9938885d5
Kai Wells <support@kaiwells.me>
2017-02-19 13:14:52 -0800

    Factor out LambertMaterials

--- RayTrace/Material.swift
+++ RayTrace/Material.swift
@@ -1,4 +1,3 @@
-import Foundation
 import simd
 
 public struct ScatterResult {
@@ -13,36 +12,4 @@ public class MaterialShader {
     }
 }
 
-public class LambertMaterial: MaterialShader {
-    var albedo: Texture
-    
-    init(albedo: Texture) {
-        self.albedo = albedo
-    }
-    
-    convenience init(color: float3) {
-        self.init(albedo: ConstantTexture(color))
-    }
-    
-    convenience init(r: Float, g: Float, b: Float) {
-        self.init(albedo: ConstantTexture(r: r, g: g, b: b))
-    }
-    
-    override func scatter(r: Ray, h: HitRecord) -> ScatterResult {
-        let scattered = Ray.RandomSphere(around: h.P)
-        let att = albedo.colorFor(u: 0, v: 0, p: h.P)
-        return ScatterResult(didScatter: true, attenuation: att, scattered: scattered)
-    }
-}
 
-public class NormLambertMaterial: LambertMaterial {
-    init() {
-        super.init(albedo: Texture())
-    }
-    
-    override func scatter(r: Ray, h: HitRecord) -> ScatterResult {
-        var result = super.scatter(r: r, h: h)
-        result.attenuation = 0.5 * (h.Normal + float3(1, 1, 1))
-        return result
-    }
-}
