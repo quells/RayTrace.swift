b8f15e07d1de54b9b55b3c914b7ddd49e67c849d
Kai Wells <support@kaiwells.me>
2017-02-19 22:30:47 -0800

    CheckerTexture

--- RayTrace/Materials/Texture.swift
+++ RayTrace/Materials/Texture.swift
@@ -21,3 +21,21 @@ public class ConstantTexture: Texture {
         return constant
     }
 }
+
+public class CheckeredTexture: Texture {
+    let even: Texture
+    let odd: Texture
+    let scale: Float
+    
+    init(even: Texture, odd: Texture, scale: Float = 10) {
+        (self.even, self.odd, self.scale) = (even, odd, scale)
+    }
+    
+    override func colorFor(u: Float, v: Float, p: float3) -> float3 {
+        let sines = sin(scale*p.x) * sin(scale*p.y) * sin(scale*p.z)
+        if sines > 0 {
+            return even.colorFor(u: u, v: v, p: p)
+        }
+        return odd.colorFor(u: u, v: v, p: p)
+    }
+}
