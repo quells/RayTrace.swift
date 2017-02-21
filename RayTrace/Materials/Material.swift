840195ba316717e1fd55ba1c736c7013680d230c
Kai Wells <support@kaiwells.me>
2017-02-20 19:10:58 -0800

    CheckeredMaterial

--- RayTrace/Materials/Material.swift
+++ RayTrace/Materials/Material.swift
@@ -27,3 +27,21 @@ public class MixedMaterial: MaterialShader {
         return b.scatter(r: r, h: h)
     }
 }
+
+public class CheckeredMaterial: MaterialShader {
+    let even: MaterialShader
+    let odd: MaterialShader
+    let scale: Float
+    
+    init(even: MaterialShader, odd: MaterialShader, scale: Float = 10) {
+        (self.even, self.odd, self.scale) = (even, odd, scale)
+    }
+    
+    override func scatter(r: Ray, h: HitRecord) -> ScatterResult? {
+        let sines = sin(scale*h.P.x) * sin(scale*h.P.y) * sin(scale*h.P.z)
+        if sines > 0 {
+            return even.scatter(r: r, h: h)
+        }
+        return odd.scatter(r: r, h: h)
+    }
+}
