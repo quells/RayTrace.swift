38388215f784c11c35b9acba53b3dc0b444b8a5c
Kai Wells <support@kaiwells.me>
2017-02-21 21:58:27 -0800

    Box

---
+++ RayTrace/Objects/Box.swift
@@ -0,0 +1,23 @@
+import simd
+
+public class Box: Object {
+    var faces: Group
+    
+    init(width: Float, height: Float, depth: Float, shader: MaterialShader) {
+        let front = Rectangle(width: width, height: height, k: 0, shader: shader)
+        let back = Rectangle(width: width, height: height, k: depth, shader: shader)
+        let a = RotateY(Rectangle(width: width, height: height, k: 0, shader: shader), angle: 90)
+        let b = RotateY(Rectangle(width: width, height: height, k: -depth, shader: shader), angle: 90)
+        let top = RotateX(Rectangle(width: width, height: height, k: 0, shader: shader), angle: 90)
+        let bottom = RotateX(Rectangle(width: width, height: depth, k: -height, shader: shader), angle: 90)
+        self.faces = Group(members: [front, back, a, b, top, bottom])
+    }
+    
+    override public func hit(r: Ray, near: Float, far: Float) -> HitRecord? {
+        return faces.hit(r: r, near: near, far: far)
+    }
+    
+    override public func boundingBox(_ tmin: Float, _ tmax: Float) -> AABB? {
+        return faces.boundingBox(tmin, tmax)
+    }
+}
